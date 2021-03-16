bf989cbf9d95086a21521d9408a88c8e1ed96490
hashimu <hashimu1@www2025.sakura.ne.jp>
2021-03-16 22:45:40 +0900

    remote push test

---
+++ public/bitbucket/config.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+/** Configuration for BitBucket Sync. */
+$CONFIG = array(
+	'commitsFolder' => 'commits',
+	'commitsFilenamePrefix' => 'commit-',
+	'automaticDeployment' => true,
+	'deployBranch' => 'master',
+	'apiUser' => 'hashimu',
+	'apiPassword' => 'konkorudo5',
+	'verbose' => true,
+	'requireAuthentication' => true,
+	'deployAuthKey' => '',
+	'gatewayAuthKey' => 'hashimu_key',
+
+);
+
+$DEPLOY = array(
+    'yugioh' => '/home/hashimu1/www/yugioh/',
+);
+
+/* Omit PHP closing tag to help avoid accidental output */
