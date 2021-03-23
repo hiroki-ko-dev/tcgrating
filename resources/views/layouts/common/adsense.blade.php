ab6044e1620da85edb7abaf9215a2958ad4961c8
康大樹 <>
2021-03-23 18:53:19 +0900

    広告コードの修正

--- resources/views/layouts/common/adsense.blade.php
+++ resources/views/layouts/common/adsense.blade.php
@@ -1,13 +1,13 @@
 @section('adsense')
 
-{{--    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>--}}
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     <ins class="adsbygoogle"
          style="display:block; text-align:center;"
          data-ad-layout="in-article"
          data-ad-format="fluid"
          data-ad-client="ca-pub-9125683895360901"
          data-ad-slot="6401800863"></ins>
-{{--    <script>--}}
-{{--        (adsbygoogle = window.adsbygoogle || []).push({});--}}
-{{--    </script>--}}
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
 @endsection
