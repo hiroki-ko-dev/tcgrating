4fe94c6d90294779d1ee4844360c8504e2d23da8
康大樹 <>
2021-03-24 01:17:46 +0900

    ログインbladeの更新

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,25 +4,33 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-6 mx-auto">
-                <div class="card card-container">
-                    <!-- <img class="profile-img-card" src="//lh3.googleusercontent.com/-6V8xOA6M7BA/AAAAAAAAAAI/AAAAAAAAAAA/rzlHcD0KYwo/photo.jpg?sz=120" alt="" /> -->
-                    <img id="profile-img" class="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
-                    <p id="profile-name" class="profile-name-card"></p>
-                    <form class="form-signin">
-                        <span id="reauth-email" class="reauth-email"></span>
-                        <input type="email" id="inputEmail" class="form-control" placeholder="ユーザーアカウント名" required autofocus>
-                        <input type="password" id="inputPassword" class="form-control" placeholder="パスワード" required>
-                        <div id="remember" class="checkbox">
-                            <label>
-                                <input type="checkbox" value="remember-me"> ログインしたままにする
-                            </label>
-                        </div>
-                        <button class="btn btn-lg btn-primary btn-block btn-signin" type="submit">Sign in</button>
-                    </form><!-- /form -->
-                    <a href="#" class="forgot-password">
-                        Forgot the password?
-                    </a>
-                </div>
+                <article class="card-body">
+                    <h4 class="card-title text-center mb-4 mt-1">ログイン画面</h4>
+                    <hr>
+                    <form>
+                        <div class="form-group">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"> <i class="fa fa-user"></i> </span>
+                                </div>
+                                <input name="" class="form-control" placeholder="Email or ユーザアカウント名" type="email">
+                            </div> <!-- input-group.// -->
+                        </div> <!-- form-group// -->
+                        <div class="form-group">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
+                                </div>
+                                <input class="form-control" placeholder="******" type="password">
+                            </div> <!-- input-group.// -->
+                        </div> <!-- form-group// -->
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary btn-block"> Login  </button>
+                        </div> <!-- form-group// -->
+                        <p class="text-center"><a href="#" class="btn">Forgot password?</a></p>
+                        <p class="text-center"><a href="#" class="btn">アカウント新規作成</a></p>
+                    </form>
+                </article>
             </div><!-- /card-container -->
         </div>
     </div><!-- /container -->
