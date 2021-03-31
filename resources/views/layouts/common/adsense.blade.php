a072f837e4ff9b59a745c3dde3e15e26794471d8
康大樹 <>
2021-03-31 22:50:35 +0900

    広告スクリプトの編集

--- resources/views/layouts/common/adsense.blade.php
+++ resources/views/layouts/common/adsense.blade.php
@@ -1,15 +1,15 @@
 @section('adsense')
 
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <ins class="adsbygoogle"
-         style="display:block; text-align:center;"
-         data-ad-layout="in-article"
-         data-ad-format="fluid"
-         data-ad-client="ca-pub-9125683895360901"
-         data-ad-slot="6401800863"></ins>
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
+{{--    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>--}}
+{{--    <ins class="adsbygoogle"--}}
+{{--         style="display:block; text-align:center;"--}}
+{{--         data-ad-layout="in-article"--}}
+{{--         data-ad-format="fluid"--}}
+{{--         data-ad-client="ca-pub-9125683895360901"--}}
+{{--         data-ad-slot="6401800863"></ins>--}}
+{{--    <script>--}}
+{{--        (adsbygoogle = window.adsbygoogle || []).push({});--}}
+{{--    </script>--}}
     {{--広告の自動適用--}}
     <script data-ad-client="ca-pub-9125683895360901" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     {{--画面上部のアンカー広告を無効にする処理--}}
