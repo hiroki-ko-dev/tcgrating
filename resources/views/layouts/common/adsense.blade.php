11458bd11027a61d0d810cdf9a561a4ed266adba
康大樹 <>
2021-03-31 22:54:33 +0900

    広告の修正

--- resources/views/layouts/common/adsense.blade.php
+++ resources/views/layouts/common/adsense.blade.php
@@ -1,23 +1,25 @@
 @section('adsense')
 
 {{--    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>--}}
-{{--    <ins class="adsbygoogle"--}}
-{{--         style="display:block; text-align:center;"--}}
-{{--         data-ad-layout="in-article"--}}
-{{--         data-ad-format="fluid"--}}
-{{--         data-ad-client="ca-pub-9125683895360901"--}}
-{{--         data-ad-slot="6401800863"></ins>--}}
+
+{{--広告の自動適用--}}
+<script data-ad-client="ca-pub-9125683895360901" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+{{--画面上部のアンカー広告を無効にする処理--}}
+<script>
+    (adsbygoogle = window.adsbygoogle || []).push({
+        google_ad_client: "ca-pub-9125683895360901",
+        enable_page_level_ads: true,
+        overlays: {bottom: true}
+    });
+</script>
+<ins class="adsbygoogle"
+         style="display:block; text-align:center;"
+         data-ad-layout="in-article"
+         data-ad-format="fluid"
+         data-ad-client="ca-pub-9125683895360901"
+         data-ad-slot="6401800863"></ins>
 {{--    <script>--}}
 {{--        (adsbygoogle = window.adsbygoogle || []).push({});--}}
 {{--    </script>--}}
-    {{--広告の自動適用--}}
-    <script data-ad-client="ca-pub-9125683895360901" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    {{--画面上部のアンカー広告を無効にする処理--}}
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({
-            google_ad_client: "ca-pub-9125683895360901",
-            enable_page_level_ads: true,
-            overlays: {bottom: true}
-        });
-    </script>
+
 @endsection
