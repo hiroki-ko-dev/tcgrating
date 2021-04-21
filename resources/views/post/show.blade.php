d57b2cab8aed7bdde4c071df551943d316628389
康大樹 <>
2021-04-21 22:06:13 +0900

    掲示板にフラッシュメッセージを追加

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -2,6 +2,14 @@
 
 @section('content')
 <div class="container">
+    <div class="row justify-content-center">
+        <!-- フラッシュメッセージ -->
+        @if (session('flash_message'))
+            <div class="text-center alert-danger rounded p-3 mb-3 col-md-7Z">
+                {{ session('flash_message') }}
+            </div>
+        @endif
+    </div>
     @include('layouts.post.post_and_comment')
 </div>
 
