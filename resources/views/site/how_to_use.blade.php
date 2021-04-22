e14cc652bbe3f2fd320411e1e59e144d1963139a
康大樹 <>
2021-04-22 20:22:39 +0900

    how_to_useの行間を変更

--- resources/views/site/how_to_use.blade.php
+++ resources/views/site/how_to_use.blade.php
@@ -3,31 +3,23 @@
 @section('content')
     <div class="container border pt-3">
         <div class="row pt-10 pb-5">
-            <div class="col-md-2">
-            </div>
             <div class="col-md-6">
                 以下の動画にて当サイトの使い方説明をご視聴ください。<br>質問やバグ報告は動画のコメント欄にしてもらって大丈夫です！<br>
             </div>
         </div>
         <div class="row pt-10 pb-5">
-            <div class="col-md-2">
-            </div>
             <div class="col-md-6">
                 <a class="navbar-brand" href="https://youtu.be/rC7oJXnaqLQ">アカウント作成・フリー掲示板の使い方動画</a><br>
             </div>
         </div>
 
         <div class="row pt-10 pb-5">
-            <div class="col-md-2">
-            </div>
             <div class="col-md-6">
                 <a class="navbar-brand" href="https://youtu.be/AIGXd5wprpo">チーム作成・チームメンバー募集掲示板の使い方</a><br>
             </div>
         </div>
 
         <div class="row pt-10 pb-5">
-            <div class="col-md-2">
-            </div>
             <div class="col-md-6">
                 <a class="navbar-brand" href="https://youtu.be/PJ8rJ5rwuhg">1vs1決闘の使い方動画</a><br>
             </div>
