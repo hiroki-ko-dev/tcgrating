5dc4b571726e4eae1488b4595d90b312b94ff5ee
康大樹 <>
2021-04-22 20:25:29 +0900

    how_to_use微修正

--- resources/views/site/how_to_use.blade.php
+++ resources/views/site/how_to_use.blade.php
@@ -9,19 +9,19 @@
         </div>
         <div class="row pt-10 pb-5">
             <div class="col-md-6">
-                <a class="navbar-brand" href="https://youtu.be/rC7oJXnaqLQ">アカウント作成・フリー掲示板の使い方動画</a><br>
+                <a href="https://youtu.be/rC7oJXnaqLQ">アカウント作成・フリー掲示板の使い方動画</a><br>
             </div>
         </div>
 
         <div class="row pt-10 pb-5">
             <div class="col-md-6">
-                <a class="navbar-brand" href="https://youtu.be/AIGXd5wprpo">チーム作成・チームメンバー募集掲示板の使い方</a><br>
+                <a href="https://youtu.be/AIGXd5wprpo">チーム作成・チームメンバー募集掲示板の使い方</a><br>
             </div>
         </div>
 
         <div class="row pt-10 pb-5">
             <div class="col-md-6">
-                <a class="navbar-brand" href="https://youtu.be/PJ8rJ5rwuhg">1vs1決闘の使い方動画</a><br>
+                <a href="https://youtu.be/PJ8rJ5rwuhg">1vs1決闘の使い方動画</a><br>
             </div>
         </div>
     </div>
