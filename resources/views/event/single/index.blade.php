bef2732173001a5e576fde1ed5aa3b18f1d71322
康大樹 <>
2021-04-26 00:29:47 +0900

    イベントインデックスのステータス表示を更新

--- resources/views/event/single/index.blade.php
+++ resources/views/event/single/index.blade.php
@@ -33,12 +33,16 @@
                                 <a href="/event/single/{{$event->id}}">vs {{$event->eventUser[0]->user->name }}</a>
                                 <span class="post-user">[対戦日時:{{$event->date}} {{$event->time}}]</span>
                                 <span class="post-user">[
-                                    @if($event->status === \APP\Models\Event::RECRUIT)
-                                        {{ __('対戦受付中') }}
-                                    @elseif($event->status === \APP\Models\Event::READY)
-                                        {{ __('マッチング済') }}
-                                    @else
-                                        {{ __('対戦完了') }}
+                                @if($event->status == \APP\Models\Event::RECRUIT )
+                                        <div class="post-user">{{ __('対戦受付中') }}</div>
+                                    @elseif($event->status == \APP\Models\Event::READY )
+                                        <div class="post-user">{{ __('マッチング済') }}</div>
+                                    @elseif($event->status == \APP\Models\Event::FINISH )
+                                        <div class="post-user">{{ __('対戦完了') }}</div>
+                                    @elseif($event->status == \APP\Models\Event::CANCEL )
+                                        <div class="post-user">{{ __('対戦キャンセル') }}</div>
+                                    @elseif($event->status == \APP\Models\Event::INVALID )
+                                        <div class="post-user">{{ __('無効試合') }}</div>
                                     @endif
                                 ]</span>
                             </div>
