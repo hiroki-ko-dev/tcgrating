d36d935d02a29d29fcd033a71fffdf77c0a736da
康大樹 <>
2021-04-28 03:21:10 +0900

    修正

--- resources/views/layouts/common/google.blade.php
+++ resources/views/layouts/common/google.blade.php
@@ -38,25 +38,14 @@
         gtag('js', new Date());
 
         gtag('config', 'UA-108878672-4');
-
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).Push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)
-                [0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-        @if(Auth::check())
-        gtag('config', 'UA-108878672-4', {
-            'user_id': '{{Auth::id()}}'
-        });
-        @else
-        gtag('config', 'UA-108878672-4', {
-            'user_id': '0'
-        });
-        @endif
-
+        
         //user-idコード
         @if(Auth::check())
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).Push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)
+                    [0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
             ga('create', 'UA-108878672-4', 'auto');
             ga('set', 'user_id', '{{Auth::id()}}'); // ログインしている user_id を使用してUser-ID を設定します。
             ga('send', 'pageview');
