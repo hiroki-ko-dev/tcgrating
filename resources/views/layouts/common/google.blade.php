622a48aa7ebcf0dc28ba90c589ce3181f4874805
康大樹 <>
2021-04-28 03:23:33 +0900

    ミス修正

--- resources/views/layouts/common/google.blade.php
+++ resources/views/layouts/common/google.blade.php
@@ -38,12 +38,12 @@
         gtag('js', new Date());
 
         gtag('config', 'UA-108878672-4');
-        
+
         //user-idコード
         @if(Auth::check())
             (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).Push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)
                     [0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
             })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
             ga('create', 'UA-108878672-4', 'auto');
