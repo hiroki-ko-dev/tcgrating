d9a1fafbc82dba6d59e552b8735fbf9af9b125c0
康大樹 <>
2021-05-06 01:12:38 +0900

    時間表示のjavascriptを修正

--- resources/views/layouts/common/footer.blade.php
+++ resources/views/layouts/common/footer.blade.php
@@ -32,16 +32,19 @@
         var day    = today.getDate();
 
         //日・時・分を取得
-        var start_hour   = today.getHours();
-        var start_minute = today.getMinutes();
+        var start_hour   = ('00' + today.getHours()).slice( -2 );
+        var start_minute = ('00' + today.getMinutes()).slice( -2 );
 
         //日・時・分を取得
-        var end_hour   = today.getHours();
-        var end_minute = today.getMinutes();
+        var end_hour   = ('00' + today.getHours() + 1).slice( -2 );
+        var end_minute = ('00' + today.getMinutes()).slice( -2 );
+
+        console.log(start_hour);
+        console.log(start_minute);
 
         document.getElementById( "datepicker" ).value = year + "/" + month + "/" + day ;
         document.getElementById( 'start_time' ).value = start_hour + ":" + start_minute ;
-        document.getElementById( 'end_time' ).value   = (end_hour + 1) + ":" + end_minute ;
+        document.getElementById( 'end_time' ).value   = end_hour + ":" + end_minute ;
     });
 
     //submitのボタン連打防止
