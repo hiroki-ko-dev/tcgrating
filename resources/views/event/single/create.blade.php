a0a1ed9a2496adadbb57ce259df07186e0e6d763
康大樹 <>
2021-05-15 14:08:30 +0900

    1vs1決闘で対戦回数の入力を必須にする

--- resources/views/event/single/create.blade.php
+++ resources/views/event/single/create.blade.php
@@ -52,7 +52,7 @@
                     <div class="card-body">
                         <div class="form-group row">
                             <div class="col-md-12">
-                                <input type="number" id="number_of_games" class="form-control w-100 @error('number_of_games') is-invalid @enderror" name="number_of_games" >{{ old('number_of_games') }}</input>
+                                <input type="number" id="number_of_games" class="form-control w-100 @error('number_of_games') is-invalid @enderror" name="number_of_games" value="{{ old('number_of_games') }}" required autocomplete="number_of_games" autofocus>
                                 @error('body')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
