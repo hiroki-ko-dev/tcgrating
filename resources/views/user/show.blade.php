4d0fc712c4d3d0c608f805f9250a0ca602e803f8
康大樹 <>
2021-07-09 12:27:57 +0900

    ユーザーページにflex対応を導入

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -36,20 +36,16 @@
             @else
                 <div class="card">
             @endif
-                <div class="card-header">{{ __('名前') }}</div>
-                <div class="card-body">
-                    <div class="form-group row">
-                        <div class="col-md-12">
-                            <div type="body">{{$user->name}}</div>
-                        </div>
-                    </div>
-                </div>
 
-                <div class="card-header">{{ __('レート') }}</div>
-                <div class="card-body">
-                    <div class="form-group row">
-                        <div class="col-md-12">
-                            <div type="body">{{$user->rate}}</div>
+                <div class="card-header">{{ __('基本情報') }}</div>
+                    <div class="card-body">
+                        <div class="d-flex flex-row mb-3">
+                            <div class="w-25 font-weight-bold">{{ __('ユーザー名') }}</div>
+                            <div class="w-75">{{$user->name}}</div>
+                        </div>
+                        <div class="d-flex flex-row">
+                            <div class="w-25 font-weight-bold">{{ __('レート') }}</div>
+                            <div class="w-75">{{number_format($user->rate_yugioh_links)}}</div>
                         </div>
                     </div>
                 </div>
