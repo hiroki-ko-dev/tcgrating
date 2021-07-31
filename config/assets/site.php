2669bd76e9dd92a9b0817f763cd87201d496f674
康大樹 <>
2021-07-31 10:25:35 +0900

    イベント作成のフォーマット変更

--- config/assets/site.php
+++ config/assets/site.php
@@ -7,5 +7,11 @@ return [
         '1' => '遊戯王デュエルリンクス',
         '2' => '遊戯王OCG',
         '3' => 'ポケモンカード'
+    ],
+
+    'game_ids' => [
+    'yugioh_duellinks' => '1',
+    'yugioh_ocg' => '2',
+    'pokemon_card' => '3'
     ]
 ];
