e4b4f1a89dc6469427864606e335c10dfd587f94
康大樹 <>
2021-09-07 20:14:16 +0900

    決闘ページのデザイン修正

--- resources/views/duel/single/show.blade.php
+++ resources/views/duel/single/show.blade.php
@@ -2,65 +2,47 @@
 
 @section('content')
 <div class="container">
-    <div class="site-color text-white rounded p-3 mb-3">
-        <h2>{{ __('1vs1対戦(対戦ページ)') }}</h2>
+  <div class="row justify-content-center m-1 mb-3">
+    <div class="col-12 page-header">
+      {{ __('1vs1対戦(対戦ページ)') }}
     </div>
+  </div>
 
     <div class="row justify-content-center">
         <!-- フラッシュメッセージ -->
         @if (session('flash_message'))
-            <div class="text-center alert-danger rounded p-3 mb-3 col-md-7Z">
+            <div class="text-center alert-danger rounded p-3 mb-3 col-12">
                 {{ session('flash_message') }}
             </div>
         @endif
     </div>
 
-    <div class="row justify-content-center mb-4">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">
-                    {{ __('対戦開始時間') }}
-                </div>
-                <div class="card-body">
-                    <div class="form-group row">
-                        <div class="col-md-12">
-                            <div class="post-body">{{date('Y/m/d H:i', strtotime($duel->eventDuel->event->date.' '.$duel->eventDuel->event->start_time))}}</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  <div class="row justify-content-center row-eq-height mb-4">
+    <div class="col-sm-6">
+      <div class="box">
+        <div class="d-flex flex-row mb-3">
+          <div class="w-30 font-weight-bold">{{ __('対戦開始時間') }}</div>
+          <div class="w-70 post-body">{{date('Y/m/d H:i', strtotime($duel->eventDuel->event->date.' '.$duel->eventDuel->event->start_time))}}</div>
         </div>
+      </div>
     </div>
-
-    <div class="row justify-content-center mb-4">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">
-                    {{ __('ルームID') }}
-                </div>
-                <div class="card-body">
-                    <div class="form-group row">
-                        <div class="col-md-12">
-                            <div class="post-body">{{$duel->room_id}}
-                                @if($duel->duelUsers[0]->user_id == Auth::id())
-                                    （<a href="/duel/single/{{$duel->id}}/edit">編集する</a>）
-                                @endif
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+    <div class="col-sm-6">
+      <div class="box">
+        <div class="d-flex flex-row mb-3">
+          <div class="w-30 font-weight-bold">{{ __('ルームID') }}</div>
+          <div class="w-70 post-body">{{$duel->room_id}}
+              @if($duel->duelUsers[0]->user_id == Auth::id())
+                  （<a href="/duel/single/{{$duel->id}}/edit">編集する</a>）
+              @endif
+          </div>
         </div>
+      </div>
     </div>
-
-    @include('layouts.post.post_and_comment')
+  </div>
 
     <div class="row justify-content-center mb-4">
         <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">
-                    {{ __('決闘詳細') }}
-                </div>
+            <div class="box">
                 <div class="card-body">
                     <div class="form-group row">
                         <div class="col-md-12">
@@ -136,7 +118,7 @@
 
     <div class="row justify-content-center mb-4">
         <div class="col-md-12">
-            <div class="card">
+            <div class="box">
                 <div class="card-header">
                     {{ $duel->games_number }} {{ __('試合目　ー　対戦結果報告') }}
                 </div>
@@ -170,6 +152,9 @@
             </div>
         </div>
     </div>
+
+    @include('layouts.post.post_and_comment')
+
 </div>
 
 @endsection
