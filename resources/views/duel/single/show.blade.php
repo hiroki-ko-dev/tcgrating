70378b2f407d3ae8e407c3352632a62ad50e3806
康大樹 <>
2021-09-09 21:54:22 +0900

    mbの位置の変更

--- resources/views/duel/single/show.blade.php
+++ resources/views/duel/single/show.blade.php
@@ -17,8 +17,8 @@
         @endif
     </div>
 
-  <div class="row justify-content-center row-eq-height mb-4">
-    <div class="col-sm-6">
+  <div class="row justify-content-center row-eq-height">
+    <div class="col-sm-6 mb-4">
       <div class="box">
         <div class="d-flex flex-row mb-3">
           <div class="w-30 font-weight-bold">{{ __('対戦開始時間') }}</div>
@@ -26,7 +26,7 @@
         </div>
       </div>
     </div>
-    <div class="col-sm-6">
+    <div class="col-sm-6 mb-4">
       <div class="box">
         <div class="d-flex flex-row mb-3">
           <div class="w-30 font-weight-bold">{{ __('ルームID') }}</div>
