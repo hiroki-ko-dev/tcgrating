82ce398bd68b39bbc6c28f20337e054a91135326
康大樹 <>
2021-09-12 07:00:21 +0900

    ユーザーページのデザイン改修

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -23,7 +23,7 @@
   <div class="row justify-content-center">
       <!-- フラッシュメッセージ -->
       @if (session('flash_message'))
-          <div class="col-md-12">
+          <div class="col-12">
               <div class="text-center alert-danger rounded p-3 mb-3">
                   {{ session('flash_message') }}
               </div>
@@ -31,95 +31,86 @@
       @endif
   </div>
 
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            @if($user->id === Auth::id())
-                <div class="card">
-                    <div class="card-header">{{ __('メールアドレス') }}</div>
-                    <div class="card-body">
-                        <div class="form-group row">
-                            <div class="col-md-12">
-                                <div type="body">{{$user->email}}</div>
-                            </div>
-                        </div>
-                    </div>
-                  @if($user->twitter_id == null)
-                    <div class="card-header">{{ __('Twitter連携') }}</div>
-                    <div class="card-body">
-                      <div class="col-md-5">
-                        <div class="d-flex flex-row mb-3">
-                          <a href="/auth/twitter/login"><img class="img-fluid" src="{{ asset('/images/site/twitter/relation.png') }}" alt="hashimu-icon"></a>
-                        </div>
-                      </div>
-                    </div>
-                  @else
-                    <div class="card-header">{{ __('Twitterアカウント') }}</div>
-                    <div class="card-body">
-                      <div class="col-md-5">
-                        <div class="d-flex flex-row mb-3">
-                          <a href="https://twitter.com/{{$user->twitter_nickname}}"><div type="body">＠{{$user->twitter_nickname}}</div></a>
-                        </div>
-                      </div>
-                    </div>
-                  @endif
-            @else
-                <div class="card">
-            @endif
-
-                <div class="card-header">{{ __('基本情報') }}</div>
-                <div class="card-body">
-                    <div class="d-flex flex-row mb-3">
-                        <div class="w-30 font-weight-bold">{{ __('ユーザー名') }}</div>
-                        <div class="w-70">{{$user->name}}</div>
-                    </div>
-                    <div class="d-flex flex-row">
-                        <div class="w-30 font-weight-bold">{{ __('レート') }}</div>
-                        <div class="w-70">
-                          @if(isset(Auth::user()->selected_game_id) && !is_null($user->gameUsers->where('game_id', Auth::user()->selected_game_id)->first()))
-                            {{number_format($user->gameUsers->where('game_id', Auth::user()->selected_game_id)->first()->rate)}}
-                          @elseif(session('selected_game_id') && $user->gameUsers->where('game_id', session('selected_game_id'))->isNotEmpty())
-                            {{number_format($user->gameUsers->where('game_id', session('selected_game_id'))->first()->rate)}}
-                          @else
-                            0
-                          @endif
-                        </div>
-                    </div>
-                </div>
-
-                <div class="card-header">{{ __('プロフィール文') }}</div>
-                <div class="card-body">
-                    <div class="form-group row">
-                        <div class="col-md-12">
-                            <div type="body">{!! nl2br(e($user->body)) !!}</div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="card-header">{{ __('参加中イベント') }}</div>
-                <div class="card-body">
-                    <div class="form-group row">
-                        <div class="col-md-12">
-                            @foreach($events as $event)
-                              @if(isset(Auth::user()->selected_game_id) && $event->game_id == Auth::user()->selected_game_id)
-                                @if($event->status == \App\Models\Event::RECRUIT)
-                                    <div type="body"><a href="/event/single/{{$event->id}}">・{{$event->title}} 対戦日時：{{date('Y/m/d H:i', strtotime($event->date.' '.$event->start_time))}}(対戦相手受付中)</a></div>
-                                @elseif($event->status == \App\Models\Event::READY)
-                                    <div type="body"><a href="/event/single/{{$event->id}}">・{{$event->title}} 対戦日時：{{date('Y/m/d H:i', strtotime($event->date.' '.$event->start_time))}}(マッチング済)</a></div>
-                                @endif
-                              @elseif(session('selected_game_id') && $user->gameUsers->where('game_id', session('selected_game_id'))->isNotEmpty() && $event->game_id == $user->gameUsers->where('game_id', session('selected_game_id'))->first()->game_id)
-                                @if($event->status == \App\Models\Event::RECRUIT)
-                                  <div type="body"><a href="/event/single/{{$event->id}}">・{{$event->title}} 対戦日時：{{date('Y/m/d H:i', strtotime($event->date.' '.$event->start_time))}}(対戦相手受付中)</a></div>
-                                @elseif($event->status == \App\Models\Event::READY)
-                                  <div type="body"><a href="/event/single/{{$event->id}}">・{{$event->title}} 対戦日時：{{date('Y/m/d H:i', strtotime($event->date.' '.$event->start_time))}}(マッチング済)</a></div>
-                                @endif
-                              @endif
-                            @endforeach
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+  <div class="row justify-content-center">
+    <div class="col-sm-6 mb-4">
+      <div class="box text-left">
+        <div class="box-header">{{ __('メールアドレス') }}</div>
+        @if($user->id === Auth::id())
+          <div class="body">{{$user->email}}</div>
+        @else
+          <div class="body">{{ __('※本人にのみ表示されます') }}</div>
+        @endif
+      </div>
     </div>
+    <div class="col-sm-6 mb-4">
+      <div class="box text-left">
+        @if($user->twitter_id == null)
+          <div class="box-header">{{ __('Twitter連携') }}</div>
+          <a href="/auth/twitter/login"><img class="img-fluid" src="{{ asset('/images/site/twitter/relation.png') }}" alt="hashimu-icon"></a>
+        @else
+          <div class="box-header">{{ __('Twitterアカウント') }}</div>
+          <a href="https://twitter.com/{{$user->twitter_nickname}}"><div type="body">＠{{$user->twitter_nickname}}</div></a>
+        @endif
+      </div>
+    </div>
+  </div>
+
+  <div class="row justify-content-center">
+    <div class="col-sm-6 mb-4">
+      <div class="box text-left">
+        <div class="box-header">{{ __('ユーザー名') }}</div>
+        <div class="d-flex flex-row mb-3">
+            <div class="body">{{$user->name}}</div>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6 mb-4">
+      <div class="box text-left">
+        <div class="box-header">{{ __('レート') }}</div>
+        <div class="body">
+          @if(isset(Auth::user()->selected_game_id) && !is_null($user->gameUsers->where('game_id', Auth::user()->selected_game_id)->first()))
+            {{number_format($user->gameUsers->where('game_id', Auth::user()->selected_game_id)->first()->rate)}}
+          @elseif(session('selected_game_id') && $user->gameUsers->where('game_id', session('selected_game_id'))->isNotEmpty())
+            {{number_format($user->gameUsers->where('game_id', session('selected_game_id'))->first()->rate)}}
+          @else
+            0
+          @endif
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="row justify-content-center">
+    <div class="col-12 mb-4">
+      <div class="box text-left">
+        <div class="box-header">{{ __('プロフィール文') }}</div>
+        <div type="body">{!! nl2br(e($user->body)) !!}</div>
+      </div>
+    </div>
+  </div>
+
+  <div class="row justify-content-center">
+    <div class="col-12 mb-4">
+      <div class="box text-left">
+        <div class="box-header">{{ __('参加中イベント') }}</div>
+        @foreach($events as $event)
+          @if(isset(Auth::user()->selected_game_id) && $event->game_id == Auth::user()->selected_game_id)
+            @if($event->status == \App\Models\Event::RECRUIT)
+              <div type="body"><a href="/event/single/{{$event->id}}">・{{$event->title}} 対戦日時：{{date('Y/m/d H:i', strtotime($event->date.' '.$event->start_time))}}(対戦相手受付中)</a></div>
+            @elseif($event->status == \App\Models\Event::READY)
+              <div type="body"><a href="/event/single/{{$event->id}}">・{{$event->title}} 対戦日時：{{date('Y/m/d H:i', strtotime($event->date.' '.$event->start_time))}}(マッチング済)</a></div>
+            @endif
+          @elseif(session('selected_game_id') && $user->gameUsers->where('game_id', session('selected_game_id'))->isNotEmpty() && $event->game_id == $user->gameUsers->where('game_id', session('selected_game_id'))->first()->game_id)
+            @if($event->status == \App\Models\Event::RECRUIT)
+              <div type="body"><a href="/event/single/{{$event->id}}">・{{$event->title}} 対戦日時：{{date('Y/m/d H:i', strtotime($event->date.' '.$event->start_time))}}(対戦相手受付中)</a></div>
+            @elseif($event->status == \App\Models\Event::READY)
+              <div type="body"><a href="/event/single/{{$event->id}}">・{{$event->title}} 対戦日時：{{date('Y/m/d H:i', strtotime($event->date.' '.$event->start_time))}}(マッチング済)</a></div>
+            @endif
+          @endif
+        @endforeach
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
 
