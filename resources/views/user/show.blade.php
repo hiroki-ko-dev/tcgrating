5b62373a058717dcf0c713e55e7888344da9bda9
康大樹 <>
2021-09-16 21:06:14 +0900

    Twitter連携のバグ修正

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -79,8 +79,12 @@
     <div class="col-sm-6 mb-4">
       <div class="box text-left">
         @if($user->twitter_id == null)
+          @if($user->id === Auth::id())
           <div class="box-header">{{ __('Twitter連携') }}</div>
           <a href="/auth/twitter/login"><img class="img-fluid" src="{{ asset('/images/site/twitter/relation.png') }}" alt="hashimu-icon"></a>
+          @else
+            ※このユーザーはTwitter未連携です
+          @endif
         @else
           <div class="box-header">{{ __('Twitterアカウント') }}</div>
           <a href="https://twitter.com/{{$user->twitter_nickname}}"><div type="body">＠{{$user->twitter_nickname}}</div></a>
