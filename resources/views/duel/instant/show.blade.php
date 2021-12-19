ce42e670445bd0fc52538e0868bbfa85b8a09be6
康大樹 <>
2021-12-20 03:15:26 +0900

    対戦作成後の画面にLINEオープンチャットの参加導線を追加

--- resources/views/duel/instant/show.blade.php
+++ resources/views/duel/instant/show.blade.php
@@ -207,6 +207,25 @@
     </div>
   </div>
 
+  @if($duel->game_id == config('assets.site.game_ids.pokemon_card') && $duel->eventDuel->event->status == \App\Models\Event::RECRUIT)
+    <div class="row justify-content-center mb-2">
+      <div class="col-md-12">
+        <div class="box">
+          <div class="row"><div class="col-12">LINEオープンチャット参加でマッチング率が飛躍的に上昇します!!</div></div>
+          <div class="row">
+            <div class="col-sm-4"> </div>
+            <div class="col-sm-4">
+              <a href="https://line.me/ti/g2/Kt5eTJpAKQ9eV-De1_m7jeJA1XLIKaQFypvEZg?utm_source=invitation&utm_medium=link_copy&utm_campaign=default">
+              <img class="img-fluid" src="{{ asset('/images/site/line.png') }}" alt="hashimu-icon"></a>
+            </div>
+            <div class="col-sm-4"> </div>
+          </div>
+          <div class="row"><div class="col-12">対戦URLをオープンチャットに貼り付けて対戦を呼びかけてみましょう!!</div></div>
+        </div>
+      </div>
+    </div>
+  @endif
+
   <div class="row justify-content-center mb-4">
     <div class="col-12">
       <div class="box">
--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -3,14 +3,19 @@
 @section('content')
     <div class="container">
       @if(session('selected_game_id') == 3)
-        <div class="row mb-2">
+        <div class="row justify-content-center mb-2">
           <div class="col-md-12">
             <div class="box">
-              <div>LINEオープンチャット参加でマッチング率が飛躍的に上昇します!!</div>
-              <div><a href="https://line.me/ti/g2/Kt5eTJpAKQ9eV-De1_m7jeJA1XLIKaQFypvEZg?utm_source=invitation&utm_medium=link_copy&utm_campaign=default">
-                  <img class="img-fluid" src="{{ asset('/images/site/line.png') }}" alt="hashimu-icon"></a>
+              <div class="row"><div class="col-12">LINEオープンチャット参加でマッチング率が飛躍的に上昇します!!</div></div>
+              <div class="row">
+                <div class="col-sm-4"> </div>
+                <div class="col-sm-4">
+                  <a href="https://line.me/ti/g2/Kt5eTJpAKQ9eV-De1_m7jeJA1XLIKaQFypvEZg?utm_source=invitation&utm_medium=link_copy&utm_campaign=default">
+                    <img class="img-fluid" src="{{ asset('/images/site/line.png') }}" alt="hashimu-icon"></a>
+                </div>
+                <div class="col-sm-4"> </div>
               </div>
-              <div>何も話さず見ているだけも可ですのでぜひご参加ください!!</div>
+              <div class="row"><div class="col-12">話さず見ているだけでOKなのでまずは参加しましょう!!</div></div>
             </div>
           </div>
         </div>
