2814fef89351f5937fae8d365f4449a365085109
康大樹 <>
2021-12-20 02:45:20 +0900

    topページにLINEオープンチャットの参加ボタンを入れる

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -2,15 +2,31 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-              @if(session('selected_game_id') == 3)
-                <img class="img-fluid" src="{{ asset('/images/site/top/003_pokemon_card.jpg') }}" alt="hashimu-icon">
-              @else
-                <img class="img-fluid" src="{{ asset('/images/site/top/001_yugioh_duellinks.jpg') }}" alt="hashimu-icon">
-              @endif
+      @if(session('selected_game_id') == 3)
+        <div class="row mb-2">
+          <div class="col-md-12">
+            <div class="box">
+              <div>LINEオープンチャット参加でマッチング率が飛躍的に上昇します!!</div>
+              <div><a href="https://line.me/ti/g2/Kt5eTJpAKQ9eV-De1_m7jeJA1XLIKaQFypvEZg?utm_source=invitation&utm_medium=link_copy&utm_campaign=default">
+                  <img class="img-fluid" src="{{ asset('/images/site/line.png') }}" alt="hashimu-icon"></a>
+              </div>
+              <div>何も話さず見ているだけも可ですのでぜひご参加ください!!</div>
             </div>
+          </div>
         </div>
+        <div class="row">
+          <div class="col-md-12">
+            <img class="img-fluid" src="{{ asset('/images/site/top/003_pokemon_card.jpg') }}" alt="hashimu-icon">
+          </div>
+        </div>
+      @else
+        <div class="row">
+          <div class="col-md-12">
+            <img class="img-fluid" src="{{ asset('/images/site/top/001_yugioh_duellinks.jpg') }}" alt="hashimu-icon">
+          </div>
+        </div>
+      @endif
+
     </div>
 
 @endsection
