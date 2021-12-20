20bab9c41bf72a9646cece3247a044700cbe5a71
康大樹 <>
2021-12-21 00:28:53 +0900

    マイページ修正

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -66,33 +66,40 @@
   </div>
 
   <div class="row justify-content-center">
-    <div class="col-sm-6 mb-4">
-      <div class="box text-left">
-        <div class="box-header">{{ __('メールアドレス') }}</div>
-        @if($user->id === Auth::id())
-          <div class="body">{{$user->email}}</div>
-        @else
-          <div class="body">{{ __('※本人にのみ表示されます') }}</div>
-        @endif
-      </div>
-    </div>
-    <div class="col-sm-6 mb-4">
-      <div class="box text-left">
-        @if($user->twitter_id == null)
+{{--    <div class="col-sm-6 mb-4">--}}
+{{--      <div class="box text-left">--}}
+{{--        <div class="box-header">{{ __('メールアドレス') }}</div>--}}
+{{--        @if($user->id === Auth::id())--}}
+{{--          <div class="body">{{$user->email}}</div>--}}
+{{--        @else--}}
+{{--          <div class="body">{{ __('※本人にのみ表示されます') }}</div>--}}
+{{--        @endif--}}
+{{--      </div>--}}
+{{--    </div>--}}
+
+    @if($user->twitter_id == null)
+      <div class="col-sm-6 mb-4">
+        <div class="box text-left">
           @if($user->id === Auth::id())
-          <div class="box-header">{{ __('Twitter連携') }}</div>
-          <a href="/auth/twitter/login"><img class="img-fluid" src="{{ asset('/images/site/twitter/relation.png') }}" alt="hashimu-icon"></a>
+            <div class="box-header">{{ __('Twitter連携') }}</div>
+            <a href="/auth/twitter/login"><img class="img-fluid" src="{{ asset('/images/site/twitter/relation.png') }}" alt="hashimu-icon"></a>
           @else
             ※このユーザーはTwitter未連携です
           @endif
-        @else
-          @if(Auth::id() == 1)
+        </div>
+      </div>
+    @else
+      @if(Auth::id() == 1)
+        <div class="col-sm-6 mb-4">
+          <div class="box text-left">
             <div class="box-header">{{ __('Twitterアカウント') }}</div>
             <a href="https://twitter.com/{{$user->twitter_nickname}}"><div type="body">＠{{$user->twitter_nickname}}</div></a>
-          @endif
-        @endif
-      </div>
-    </div>
+          </div>
+        </div>
+      @endif
+    @endif
+
+
   </div>
 
   <div class="row justify-content-center">
