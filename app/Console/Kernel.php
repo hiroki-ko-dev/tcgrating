19504c5d01227bc04a6ce30defcef0740a4d0269
康大樹 <>
2022-02-16 11:32:38 +0900

    バッチを12時に実行するかテスト設定

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('command:tweetPromotion')->dailyAt('11:12');
+        $schedule->command('command:tweetPromotion')->dailyAt('12:00');
     }
 
     /**
