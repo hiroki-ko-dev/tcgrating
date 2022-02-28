f711bcc010e255d4d7daac115fa93446ff5cfdd0
hashimuman <hiroki.ko.mail@gmail.com>
2022-02-28 17:07:16 +0900

    元に戻す

--- resources/views/duel/instant/show.blade.php
+++ resources/views/duel/instant/show.blade.php
@@ -257,6 +257,17 @@
   </div>
 </div>
 
+<script>
+  function copyUrl() {
+    const element = document.createElement('input');
+    element.value = location.href;
+    document.body.appendChild(element);
+    element.select();
+    document.execCommand('copy');
+    document.body.removeChild(element);
+  }
+</script>
+
 @endsection
 
 
