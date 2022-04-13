5d77e65eae0be361c7d97e1b644bd23e9208c607
hashimuman <hiroki.ko.mail@gmail.com>
2022-04-13 20:25:18 +0900

    seederの修正

--- database/seeders/VideoTableSeeder.php
+++ database/seeders/VideoTableSeeder.php
@@ -15,15 +15,13 @@ class VideoTableSeeder extends Seeder
      */
     public function run()
     {
-        for($i=0;$i<10;$i++){
-            Video::insert([
-                'game_id'   => 3,
-                'title'    => '動画のタイトル',
-                'thumbnail_image_url' => 'https://pbs.twimg.com/media/FJERxyLVUAER4T6?format=jpg&name=large',
-                'url'     => 'https://youtu.be/8JcpOitXWHQ',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ]);
-        }
+        Video::insert([
+            'game_id'   => 3,
+            'title'    => '動画のタイトル',
+            'thumbnail_image_url' => 'https://pbs.twimg.com/media/FJERxyLVUAER4T6?format=jpg&name=large',
+            'url'     => 'https://youtu.be/8JcpOitXWHQ',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
     }
 }
