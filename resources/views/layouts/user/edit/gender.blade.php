9274e2120b6c6844c99d05f155580524b5041fd6
hashimuman <hiroki.ko.mail@gmail.com>
2022-05-21 21:31:56 +0900

    radioをFormから変更

--- resources/views/layouts/user/edit/gender.blade.php
+++ resources/views/layouts/user/edit/gender.blade.php
@@ -4,15 +4,21 @@
     <div class="form-group row">
       <div class="col-md-12">
         <label class="radio-button">
-          {{ Form::radio('gender', 1, (old('gender',$user->gender)== 1 ? true : false), ['class' => 'radio-button__input']) }}
+          <input type="radio" name="gender" value="1" class="radio-button__input"
+                 @if(old('gender',$user->gender)== 1 ? true : false) checked @endif
+          >
           <span class="radio-button__icon">男</span>
         </label>
         <label class="radio-button">
-          {{ Form::radio('gender', 2,  (old('gender',$user->gender)== 2 ? true : false), ['class' => 'radio-button__input']) }}
+          <input type="radio" name="gender" value="2" class="radio-button__input"
+                 @if(old('gender',$user->gender)== 2 ? true : false) checked @endif
+          >
           <span class="radio-button__icon">女</span>
         </label>
         <label class="radio-button">
-          {{ Form::radio('gender', 0, (old('gender',$user->gender)== 0 ? true : false), ['class' => 'radio-button__input']) }}
+          <input type="radio" name="gender" value="0" class="radio-button__input"
+                 @if(old('gender',$user->gender)== 0 ? true : false) checked @endif
+          >
           <span class="radio-button__icon">回答しない</span>
         </label>
       </div>
