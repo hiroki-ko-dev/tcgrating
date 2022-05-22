99df181b7f11ad9ff2ea6b1186f826356841f008
hashimuman <hiroki.ko.mail@gmail.com>
2022-05-22 22:52:44 +0900

    landingページにurlを追加

--- resources/views/site/landing/03_pokemon_card.blade.php
+++ resources/views/site/landing/03_pokemon_card.blade.php
@@ -26,10 +26,14 @@
           <div class="font-weight-bold mb-2 download">アプリをダウンロード！！</div>
           <div class="d-flex flex-row mb-3">
             <div class="w-50 rounded m-1 pb-2">
-              <img class="img-fluid iphone" src="{{ asset('/images/site/landing/iphone_download.png') }}" alt="hashimu-icon">
+              <a href="https://apps.apple.com/us/app/tcgrating/id1620838810?itsct=apps_box_link&itscg=30200">
+                <img class="img-fluid iphone" src="{{ asset('/images/site/landing/iphone_download.png') }}" alt="hashimu-icon">
+              </a>
             </div>
             <div class="w-50 rounded m-1 pb-2">
-              <img class="img-fluid" src="{{ asset('/images/site/landing/android_download.png') }}" alt="hashimu-icon">
+              <a href="https://play.google.com/store/apps/details?id=hashimu.tcgrating&hl=ja">
+                <img class="img-fluid" src="{{ asset('/images/site/landing/android_download.png') }}" alt="hashimu-icon">
+              </a>
             </div>
           </div>
         </div>
@@ -136,10 +140,14 @@
           <div class="font-weight-bold mb-2 download">アプリをダウンロード！！</div>
           <div class="d-flex flex-row mb-3">
             <div class="w-50 rounded m-1 pb-2">
-              <img class="img-fluid iphone" src="{{ asset('/images/site/landing/iphone_download.png') }}" alt="hashimu-icon">
+              <a href="https://apps.apple.com/us/app/tcgrating/id1620838810?itsct=apps_box_link&itscg=30200">
+                <img class="img-fluid iphone" src="{{ asset('/images/site/landing/iphone_download.png') }}" alt="hashimu-icon">
+              </a>
             </div>
-            <div class="w-50 rounded m-1 pb-5">
-              <img class="img-fluid" src="{{ asset('/images/site/landing/android_download.png') }}" alt="hashimu-icon">
+            <div class="w-50 rounded m-1 pb-2">
+              <a href="https://play.google.com/store/apps/details?id=hashimu.tcgrating&hl=ja">
+                <img class="img-fluid" src="{{ asset('/images/site/landing/android_download.png') }}" alt="hashimu-icon">
+              </a>
             </div>
           </div>
         </div>
