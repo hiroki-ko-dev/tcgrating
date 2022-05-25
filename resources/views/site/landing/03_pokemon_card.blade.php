b62716eb47c02971e944f51efed2ec51bed0fa2f
hashimuman <hiroki.ko.mail@gmail.com>
2022-05-25 21:36:23 +0900

    ランディグページをアプリ→lineに変更

--- resources/views/site/landing/03_pokemon_card.blade.php
+++ resources/views/site/landing/03_pokemon_card.blade.php
@@ -16,29 +16,9 @@
       </div>
     </div>
 
-    {{--    <div class="pc">--}}
-    {{--      @include('layouts.common.line')--}}
-    {{--    </div>--}}
+      @include('layouts.common.line')
 
-    <div class="row justify-content-center mobile mb-4 mt-2">
-      <div class="col-12">
-        <div class="box">
-          <div class="font-weight-bold mb-2 download">アプリをダウンロード！！</div>
-          <div class="d-flex flex-row mb-3">
-            <div class="w-50 rounded m-1 pb-2">
-              <a href="https://apps.apple.com/us/app/tcgrating/id1620838810?itsct=apps_box_link&itscg=30200">
-                <img class="img-fluid iphone" src="{{ asset('/images/site/landing/iphone_download.png') }}" alt="hashimu-icon">
-              </a>
-            </div>
-            <div class="w-50 rounded m-1 pb-2">
-              <a href="https://play.google.com/store/apps/details?id=hashimu.tcgrating&hl=ja">
-                <img class="img-fluid" src="{{ asset('/images/site/landing/android_download.png') }}" alt="hashimu-icon">
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+{{--    @include('layouts.common.application')--}}
 
     <div class="row justify-content-center">
       <div class="col-12">
@@ -130,29 +110,9 @@
     {{--      </div>--}}
     {{--    </div>--}}
 
-    {{--    <div class="mobile">--}}
-    {{--      @include('layouts.common.line')--}}
-    {{--    </div>--}}
+          @include('layouts.common.line')
 
-    <div class="row justify-content-center mobile mb-5">
-      <div class="col-12">
-        <div class="box">
-          <div class="font-weight-bold mb-2 download">アプリをダウンロード！！</div>
-          <div class="d-flex flex-row mb-3">
-            <div class="w-50 rounded m-1 pb-2">
-              <a href="https://apps.apple.com/us/app/tcgrating/id1620838810?itsct=apps_box_link&itscg=30200">
-                <img class="img-fluid iphone" src="{{ asset('/images/site/landing/iphone_download.png') }}" alt="hashimu-icon">
-              </a>
-            </div>
-            <div class="w-50 rounded m-1 pb-2">
-              <a href="https://play.google.com/store/apps/details?id=hashimu.tcgrating&hl=ja">
-                <img class="img-fluid" src="{{ asset('/images/site/landing/android_download.png') }}" alt="hashimu-icon">
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+{{--    @include('layouts.common.application')--}}
 
   </div>
 
