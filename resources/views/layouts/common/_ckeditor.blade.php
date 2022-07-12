c97a9f1e491b04d67c90060cfa0db3320a660b42
hashimuman <hiroki.ko.mail@gmail.com>
2022-07-12 14:54:31 +0900

    ブログデザインの修正

--- resources/views/layouts/common/_ckeditor.blade.php
+++ resources/views/layouts/common/_ckeditor.blade.php
@@ -7,29 +7,31 @@
 
   CKEDITOR.replace('editor', {
     contentsCss: [
-      '/css/blog/show.css'
-    ]
+      '/css/blog/show.css',
+    ],
+    stylesSet: [
+      { name: 'Big',              element: 'big' },
+      { name: 'Small',            element: 'small' },
+      { name: 'Typewriter',       element: 'tt' },
+
+      { name: 'Blue Title',       element: 'h3',      styles: { 'color': 'Blue' } },
+      { name: 'Red Title',        element: 'h3',      styles: { 'color': 'Red' } },
+
+      // Inline Styles
+      { name: 'Marker: Yellow',   element: 'span',    styles: { 'background-color': 'Yellow' } },
+      { name: 'Marker: Green',    element: 'span',    styles: { 'background-color': 'Lime' } },
+
+      // Object Styles
+      { name: 'effect', element: 'div', attributes: { class: 'effect' },
+        styles: {
+            color: '#000000',
+            background: '#dcdcdc',
+            border: 'solid 2px #000000',
+            padding: '5px'
+        },
+      },
+    ],
   });
 
-  // CKEDITOR.stylesSet.add( 'default', [
-  //   // Block Styles
-  //   { name: 'List', element: 'span',  styles: { 'border': 'solid 1px #000000', 'background': '#E6E6E6', 'padding': '0.5em 3em', 'margin': '1em 0' }},
-  // //   // { name: 'List',       element: 'h3',      styles: { 'color': 'Blue' }, attributes: { class: 'some-class' }},
-  // //   // { name: 'Red Title',        element: 'h3',      styles: { 'color': 'Red' }, attributes: { class: 'some-class' }},
-  // //   //
-  // //   // // Inline Styles
-  // //   // { name: 'Marker: Yellow',   element: 'span',    styles: { 'background-color': 'Yellow' } },
-  // //   // { name: 'Marker: Green',    element: 'span',    styles: { 'background-color': 'Lime' } },
-  // //   //
-  // //   // Object Styles
-  // //   // {
-  // //   //   name: 'Image on Left',
-  // //   //   element: 'img',
-  // //   //   attributes: {
-  // //   //     style: 'padding: 5px; margin-right: 5px',
-  // //   //     border: '2',
-  // //   //     align: 'left'
-  // //   //   }
-  // //   // }
-  // ] );
+
 </script>
