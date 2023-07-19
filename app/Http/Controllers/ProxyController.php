668d96cd6c18a1d0e05c742221d746db25551846
hko <hko.address@gmail.com>
2023-07-19 14:32:53 +0900

    PDFの修正

--- app/Http/Controllers/ProxyController.php
+++ app/Http/Controllers/ProxyController.php
@@ -2,24 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use Barryvdh\DomPDF\Facade as PDF;
+use Barryvdh\DomPDF\PDF;
 use Illuminate\Http\Request;
 
 class ProxyController extends Controller
 {
-
-    public function pdf(Request $request)
+    public function pdf(Request $request, PDF $pdf)
     {
         $viewImages = [];
-        foreach($request->proxy_number as $i => $number){
-            if($number > 0){
-                for($j=1; $j<=$number; $j++){
+        foreach ($request->proxy_number as $i => $number) {
+            if ($number > 0) {
+                for ($j = 1; $j <= $number; $j++) {
                     $viewImages[] = $request->proxy_url[$i];
                 }
             }
         }
-
-        $pdf = PDF::loadView('proxy.pdf',compact('viewImages'));
+        $pdf = $pdf->loadView('proxy.pdf', compact('viewImages'));
 
         return $pdf->stream('プロキシ画像.pdf');
     }
