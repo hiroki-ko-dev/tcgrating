419366654b06197b2c695e299e5327f6072e86d0
hko <hko.address@gmail.com>
2023-07-30 23:38:32 +0900

    不要な定数を削除

--- app/Models/EventUser.php
+++ app/Models/EventUser.php
@@ -8,60 +8,49 @@ use Illuminate\Database\Eloquent\Model;
 class EventUser extends Model
 {
     use HasFactory;
+
     protected $guarded = [];
 
     //定数の定義
-    const STATUS_REQUEST   = 0;
-    const STATUS_APPROVAL  = 1;
-    const STATUS_REJECT    = 2;
-    const STATUS_MASTER    = 3;
-
-    const STATUS = [
-        'request'  => self::STATUS_REQUEST,
-        'approval' => self::STATUS_APPROVAL,
-        'reject'   => self::STATUS_REJECT,
-        'master'   => self::STATUS_MASTER,
-    ];
-
-    //定数の定義
-    const ATTENDANCE_PREPARING = 0;
-    const ATTENDANCE_READY     = 1;
-    const ATTENDANCE_ATTENDED  = 2;
-    const ATTENDANCE_ABSENT    = 3;
-    const ATTENDANCE = [
+    public const ATTENDANCE_PREPARING = 0;
+    public const ATTENDANCE_READY     = 1;
+    public const ATTENDANCE_ATTENDED  = 2;
+    public const ATTENDANCE_ABSENT    = 3;
+    public const ATTENDANCE = [
         'preparing'  => self::ATTENDANCE_PREPARING,
         'ready'      => self::ATTENDANCE_READY,
         'attended'   => self::ATTENDANCE_ATTENDED,
         'absent'     => self::ATTENDANCE_ABSENT,
     ];
 
-    const ROLE_USER   = 0;
-    const ROLE_ADMIN  = 1;
-
-    const ROLE = [
+    public const ROLE_USER   = 0;
+    public const ROLE_ADMIN  = 1;
+    public const ROLE = [
         'user'  => self::ROLE_USER,
         'admin' => self::ROLE_ADMIN,
     ];
 
     public function getEventRateAttribute()
     {
-        if(in_array($this->event->now_match_number, [0,1])){
+        if (in_array($this->event->now_match_number, [0,1])) {
             return 0;
-        }else{
-            $duel_user_ids = DuelUser::whereIn('duel_id',$this->event->eventDuels->pluck('duel_id'))
-                                    ->where('user_id',$this->user_id)
-                                    ->pluck('id');
+        } else {
+            $duel_user_ids = DuelUser::whereIn('duel_id', $this->event->eventDuels->pluck('duel_id'))
+                                ->where('user_id', $this->user_id)
+                                ->pluck('id');
 
             $rate = DuelUserResult::whereIn('duel_user_id', $duel_user_ids)->sum('rating');
             return $rate;
         }
     }
 
-    public function event(){
-        return $this->belongsTo('App\Models\Event','event_id','id');
+    public function event()
+    {
+        return $this->belongsTo('App\Models\Event', 'event_id', 'id');
     }
 
-    public function user(){
-        return $this->belongsTo('App\Models\User','user_id','id');
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User', 'user_id', 'id');
     }
 }
