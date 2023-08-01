f388e37d903796917b964110cc9070284973f7c1
hko <hko.address@gmail.com>
2023-08-01 22:37:40 +0900

    削除

--- app/Models/DuelUserResult.php
+++ app/Models/DuelUserResult.php
@@ -10,17 +10,4 @@ class DuelUserResult extends Model
     use HasFactory;
 
     protected $guarded = [];
-
-    //定数の定義
-    const RESULT_WIN     = 1;
-    const RESULT_LOSE    = 2;
-    const RESULT_DRAW    = 3;
-    const RESULT_INVALID = 4;
-
-    const RESULT = [
-        'win'     => self::RESULT_WIN,
-        'lose'    => self::RESULT_LOSE,
-        'draw'    => self::RESULT_DRAW,
-        'invalid' => self::RESULT_INVALID,
-    ];
 }
