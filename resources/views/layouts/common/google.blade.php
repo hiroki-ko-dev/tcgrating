9ba392a33f82b9132b561d4a11dcc50125e19482
hko <hko.address@gmail.com>
2023-11-03 00:10:38 +0900

    adsenseをビジネス用に切り替え

--- resources/views/layouts/common/google.blade.php
+++ resources/views/layouts/common/google.blade.php
@@ -65,22 +65,6 @@
 @endsection
 
 @section('adsense')
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    {{--    <ins class="adsbygoogle"--}}
-    {{--         style="display:block; text-align:center;"--}}
-    {{--         data-ad-layout="in-article"--}}
-    {{--         data-ad-format="fluid"--}}
-    {{--         data-ad-client="ca-pub-9125683895360901"--}}
-    {{--         data-ad-slot="6401800863"></ins>--}}
-
-    {{--広告の自動適用--}}
-    {{--    <script data-ad-client="ca-pub-9125683895360901" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>--}}
-    {{--画面上部のアンカー広告を無効にする処理--}}
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({
-            google_ad_client: "ca-pub-9125683895360901",
-            enable_page_level_ads: true,
-            overlays: {bottom: true}
-        });
-    </script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9246343628014182"
+     crossorigin="anonymous"></script>
 @endsection
