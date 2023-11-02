544ef9300347ca6b5332bd0eecca5f8617600879
hko <hko.address@gmail.com>
2023-11-03 00:58:28 +0900

    修正

--- config/assets/twitter.php
+++ config/assets/twitter.php
@@ -22,10 +22,11 @@ return [
     ],
 
     'best_sale_book' => [
-        'API_KEY'                 => 'ZDnyRSOKoitFmw1ocXvZ72WVv',
-        'API_SECRET'              => 'jmjnoHrt0casvnMh7rFtMKTjM1sJ1GaKployMV02e7DFN5GtkN',
-        'API_ACCESS_TOKEN'        => '1575033361382268928-nvDOXBpd69GRxLu9B73Uh0VU0RikRS',
-        'API_ACCESS_TOKEN_SECRET' => 'Va6VCKaZXSj4rN1tLP1iuDIxbsBftgjuTS8GReUmJs8vb',
+        'API_KEY'                 => 'QZVpZ0jTQ5oHLgdDTtEof0Agi',
+        'API_SECRET'              => 'uxWgTuMVjgu7wE0YCNSrxDLzmu2H79Bj8eGasJ3riRPMSSHNV0',
+        'API_ACCESS_TOKEN'        => '1575033361382268928-4hz5AEBDLmDCrI5olByHhILZBb5HdR',
+        'API_ACCESS_TOKEN_SECRET' => 'onoJc38vhu2ZFsUHvxiGSIpgRyDRK31sY8UPFjnspVXGy',
+        'BEARER_TOKEN' => 'AAAAAAAAAAAAAAAAAAAAAMBMqwEAAAAAkh7kDIDSOBNuxFXNVZuFwkzRpuY%3DfyVOmtlhKSvl4G83Wjux6eqwDNTHeQztEQwtIpGPztNdl5WwfW',
     ],
 
     'gravure' => [
@@ -33,7 +34,7 @@ return [
         'API_SECRET'              => 'lIMmZxLhBpg0VmQlLzYFiuoUACHLd7AVramqIpNGLwoqBCUewC',
         'API_ACCESS_TOKEN'        => '1575010027093434368-laLd0LoBOIe2BTxdwP5xE2LYHD4w5F',
         'API_ACCESS_TOKEN_SECRET' => 'CUrkuUXAsCAprzc19Mo5rHkjb2Q1quFPup1WBRtwIxYMc',
-        'Bearer_Token' => 'AAAAAAAAAAAAAAAAAAAAAJtMqwEAAAAATnsNe3r4QmPugDBiNF9Q5patCJM%3DKXn1nQyG8lsvOY6XpMjRKq02Z9ICWVa4BwS6wtVxPePkgG4sTR',
+        'BEARER_TOKEN' => 'AAAAAAAAAAAAAAAAAAAAAJtMqwEAAAAATnsNe3r4QmPugDBiNF9Q5patCJM%3DKXn1nQyG8lsvOY6XpMjRKq02Z9ICWVa4BwS6wtVxPePkgG4sTR',
     ],
 
 ];
