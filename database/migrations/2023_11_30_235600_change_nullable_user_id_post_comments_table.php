d6d5953b5627ef070913de9a94100d8d30f44dd4
hko <hko.address@gmail.com>
2023-12-04 00:30:04 +0900

    修正

--- database/migrations/2023_11_30_235600_change_nullable_user_id_post_comments_table.php
+++ database/migrations/2023_11_30_235600_change_nullable_user_id_post_comments_table.php
@@ -16,7 +16,9 @@ class ChangeNullableUserIdPostCommentsTable extends Migration
         //外部キー制約を一旦無効化
         Schema::disableForeignKeyConstraints();
         Schema::table('post_comments', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
             $table->bigInteger('user_id')->nullable(true)->change();
+            $table->foreign('user_id')->references('id')->on('users');
         });
         //外部キー制約を有効化
         Schema::enableForeignKeyConstraints();
@@ -32,7 +34,9 @@ class ChangeNullableUserIdPostCommentsTable extends Migration
         //外部キー制約を一旦無効化
         Schema::disableForeignKeyConstraints();
         Schema::table('post_comments', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
             $table->bigInteger('user_id')->nullable(false)->change();
+            $table->foreign('user_id')->references('id')->on('users');
         });
         //外部キー制約を有効化
         Schema::enableForeignKeyConstraints();
