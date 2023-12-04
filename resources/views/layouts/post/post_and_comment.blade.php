a051cb1d49b3f3a39a0e24a46b56b2c17878b515
hko <hko.address@gmail.com>
2023-12-04 12:21:49 +0900

    掲示板　デザイン修正

--- resources/views/layouts/post/post_and_comment.blade.php
+++ resources/views/layouts/post/post_and_comment.blade.php
@@ -5,12 +5,12 @@
       <div class="box text-left">
 
         <section>
-          <div class="pb-2">
-            <div class="bg-pink pt-2 pb-2 d-flex align-items-center">
-                <span class="text-nowrap bg-info rounded-pill text-white p-1">
-                  {{$post->subCategoryName}}
-                </span>
-                <h1 class="text-wrap pl-1">{{$post->title}}</h1>
+          <div class="pb-1">
+            <div class="pt-2 pb-2 d-flex align-items-center">
+              <span class="text-nowrap bg-info rounded-pill text-white p-1">
+                {{$post->subCategoryName}}
+              </span>
+              <h1 class="text-wrap post-title p-2">{{$post->title}}</h1>
             </div>
             <nav>
               <div class="form-group row">
@@ -18,10 +18,16 @@
                   <span class="post-user">1. [{{$post->createdAt}}]</span>
                   <a href="/post/comment/create?post_id={{$post->id}}">返信する</a>
                 </div>
-                <div class="row pl-3">
+                <div class="row pt-3 pl-3 pr-3">
                   <div class="col-md-12">
-                    <img src="{{$post->user->profileImagePath}}" class="rounded-circle" onerror="this.src='{{ asset('/images/icon/default-account.png') }}'">
-                    <a class="font-weight-bold" href="/resume/{{$post->user->id}}">{{$post->user->name}}</a>
+                    <img src="{{$post->user->profileImagePath}}" class="rounded-circle small-profile" onerror="this.src='{{ asset('/images/icon/default-account.png') }}'">
+                    <span class="post-user">
+                      @if($post->user->id)
+                        <a class="font-weight-bold" href="/resume/{{$post->user->id}}">{{$post->user->name}}</a>
+                      @else
+                        {{$post->user->name}}
+                      @endif
+                    </span>
                   </div>
                 </div>
               </div>
@@ -58,67 +64,71 @@
         @if(!empty($post->comments))
           <section>
             <div class="pb-5 mt-3">
-                <div class="card-header">{{ __('コメント一覧') }}</div>
-                <div>
-                  @if(empty($post->comments))
-                    <div class="post-text text-secondary p-2">
-                      現在コメントはありません
+              <div class="border-bottom"></div>
+              <div>
+                @if(empty($post->comments))
+                  <div class="post-text text-secondary p-2">
+                    現在コメントはありません
+                  </div>
+                @else
+                  @foreach($post->comments as $comment)
+                    <div class="pt-3 pb-2">
+                      <div class="pt-1 pb-2">
+                        <span class="post-user">{{$comment->number}}. [{{$comment->createdAt}}]</span>
+                        <a href="/post/comment/create?comment_id={{$comment->id}}">返信する</a>
+                      </div>
+                      <img src="{{$comment->user->profileImagePath}}" class="rounded-circle small-profile" onerror="this.src='{{ asset('/images/icon/default-account.png') }}'">
+                      <span class="post-user">
+                        @if($comment->user->id)
+                          <a href="/resume/{{$comment->userId}}">{{$comment->user->name}}</a>
+                        @else
+                          {{$comment->user->name}}
+                        @endif                     
+                      </span>
                     </div>
-                  @else
-                    @foreach($post->comments as $comment)
-                        <div class="pt-3">
-                          <div>
-                            <span class="post-user">{{$comment->number}}. [{{$comment->createdAt}}]</span>
-                            <a href="/post/comment/create?comment_id={{$comment->id}}">返信する</a>
-                          </div>
-                          <img src="{{$comment->user->profileImagePath}}" class="rounded-circle" onerror="this.src='{{ asset('/images/icon/default-account.png') }}'">
-                          <span class="post-user"><a href="/resume/{{$comment->userId}}">{{$comment->user->name}}</a></span>
+                    @if(!empty($comment->imageUrl))
+                      <div class="form-group row mt-2">
+                        <div class="col-12">
+                          デッキコード：<a href="https://www.pokemon-card.com/deck/confirm.html/deckID/{{$comment->imageUrl}}">{{$comment->imageUrl}}</a>
                         </div>
-                        @if(!empty($comment->imageUrl))
-                          <div class="form-group row mt-2">
-                            <div class="col-12">
-                              デッキコード：<a href="https://www.pokemon-card.com/deck/confirm.html/deckID/{{$comment->imageUrl}}">{{$comment->imageUrl}}</a>
-                            </div>
-                          </div>
-                          <div class="form-group row mt-2">
-                            <div class="col-md-12">
-                              <img class="img-fluid" src="https://www.pokemon-card.com/deck/deckView.php/deckID/{{$comment->imageUrl}}" alt="{{$post->title}}">
-                            </div>
-                          </div>
-                        @endif
-                        @if($comment->isReferralPost || $comment->referralComment )
-                          <div class="row mt-2">
-                            <div class="bg-skyblue p-1 ml-3 font-weight-bold text-primary">
-                              @if($comment->isReferralPost)
-                                <a href="/post/comment/create?post_id={{$comment->postId}}">>>1</a>
-                              @else
-                                <a href="/post/comment/create?comment_id={{$comment->referralComment->id}}">>>{{$comment->referralComment->number}}</a>
-                              @endif
-                            </div>
-                          </div>
-                        @endif
-                        <div class="post-text pb-2">
-                            {!! nl2br(e($comment->body)) !!}
+                      </div>
+                      <div class="form-group row mt-2">
+                        <div class="col-md-12">
+                          <img class="img-fluid" src="https://www.pokemon-card.com/deck/deckView.php/deckID/{{$comment->imageUrl}}" alt="{{$post->title}}">
                         </div>
-                        @if($comment->replyCommentCount)
-                          <div class="row mt-2 pb-2">
-                            <div class="bg-pink p-1 ml-3 font-weight-bold">
-                              <a href="/post/comment/create?comment_id={{$comment->id}}" class="text-redPurple">{{$comment->replyCommentCount}}件の返信</a>
-                            </div>
-                          </div>
-                        @endif
-                      <div class="border-bottom"></div>
+                      </div>
+                    @endif
+                    @if($comment->isReferralPost || $comment->referralComment )
+                      <div class="row mt-2">
+                        <div class="bg-skyblue p-1 ml-3 font-weight-bold text-primary">
+                          @if($comment->isReferralPost)
+                            <a href="/post/comment/create?post_id={{$comment->postId}}">>>1</a>
+                          @else
+                            <a href="/post/comment/create?comment_id={{$comment->referralComment->id}}">>>{{$comment->referralComment->number}}</a>
+                          @endif
+                        </div>
+                      </div>
+                    @endif
+                    <div class="post-text pb-4">
+                        {!! nl2br(e($comment->body)) !!}
+                    </div>
+                    @if($comment->replyCommentCount)
+                      <div class="row mt-2 pb-2">
+                        <div class="bg-pink p-1 ml-3 font-weight-bold">
+                          <a href="/post/comment/create?comment_id={{$comment->id}}" class="text-redPurple">{{$comment->replyCommentCount}}件の返信</a>
+                        </div>
+                      </div>
+                    @endif
+                    <div class="border-bottom"></div>
                   @endforeach
-                  @endif
-                </div>
+                @endif
+              </div>
             </div>
           </section>
         @endif
 
         {{$post->comments->links('pagination::bootstrap-4')}}
-        <div class="card-header">
-            {{ __('コメントを投稿する' )}}
-        </div>
+        <div class="sub-title">コメントを投稿する</div>
         <div class="card-body">
           <form method="POST" action="/post/comment">
             @csrf
@@ -149,12 +159,12 @@
               </div>
             @endif
             <div class="form-group row mb-0">
-                <div class="col-md-6 offset-md-5">
-                    <button type="submit" class="btn site-color rounded-pill btn-outline-secondary text-light pl-5 pr-5" onClick="return requestConfirm();">
-                        {{ __('投稿') }}
-                    </button>
-                    <a href="/reload">　　返信を確認</a>
-                </div>
+              <div class="col-md-6 offset-md-5">
+                <button type="submit" class="btn site-color btn-outline-secondary text-light w-40 mr-2" onClick="return requestConfirm();">
+                  {{ __('投稿') }}
+                </button>
+                <button class="btn bg-secondary text-white w-40" onClick="history.back()">戻る</button>
+              </div>
             </div>
           </form>
         </div>
