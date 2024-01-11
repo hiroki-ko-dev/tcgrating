94ddc8e2f9e50663c7eee71d99c2b0bf279d2e4d
hko <hko.address@gmail.com>
2024-01-11 18:54:32 +0900

    blogの前次を上部にも表示

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -36,6 +36,10 @@
 
   @include('layouts.post.post_latest')
 
+  <section>
+    @include('layouts.blog.before_next')
+  </section>
+
   <article>
     <section>
       <div class="row justify-content-center mb-2">
@@ -55,52 +59,9 @@
       </div>
     </section>
 
-    <nav>
-      <div class="row justify-content-center mb-1">
-        <div class="col-md-5 col-6 pr-1">
-          <div class="box">
-            <div class="row justify-content-center mb-4">
-              前の記事
-            </div>
-            @if($preview)
-              <a href="/blogs/{{$preview->id}}">
-                <div class="row justify-content-center mb-4">
-                {{$preview->title}}
-                </div>
-                <div class="row justify-content-center mb-4">
-                  <img class="thumbnail" src="{{ $preview->thumbnail_image_url }}" alt="hashimu-icon">
-                </div>
-              </a>
-            @else
-              <div class="row justify-content-center mb-4">
-                記事がありません
-              </div>
-            @endif
-          </div>
-        </div>
-        <div class="col-md-5 col-6 pl-1">
-          <div class="box">
-            <div class="row justify-content-center mb-4">
-              次の記事
-            </div>
-            @if($next)
-              <a href="/blogs/{{$next->id}}">
-                <div class="row justify-content-center mb-4">
-                  <a href="/blogs/{{$next->id}}">{{$next->title}}</a>
-                </div>
-                <div class="row justify-content-center mb-4">
-                  <img class="thumbnail" src="{{ $next->thumbnail_image_url }}" alt="hashimu-icon">
-                </div>
-              </a>
-            @else
-              <div class="row justify-content-center mb-4">
-                更新中
-              </div>
-            @endif
-          </div>
-        </div>
-      </div>
-    </nav>
+    <section>
+      @include('layouts.blog.before_next')
+    </section>
 
     <section>
       @include('layouts.common.line')
