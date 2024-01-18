87b0beda904c64a85686b670b6c6189aaeac3caf
hko <hko.address@gmail.com>
2024-01-19 00:46:21 +0900

    tag manegerを仕込む

--- resources/views/layouts/common/common.blade.php
+++ resources/views/layouts/common/common.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html >
 <html class="no-js no-svg" lang="ja">
   <head>
+    @if(config('assets.common.appEnv') == 'production')
+      @yield('tag_manager_header')
+      {{-- @yield('analitics') --}}
+    @endif
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9246343628014182"
      crossorigin="anonymous"></script>
-    @if(config('assets.common.appEnv') == 'production')
-      {{-- @yield('tag_manager_header') --}}
-      @yield('analitics')
-    @endif
 
       {{--メタタグ--}}
     <title>{{config('assets.site.title')}} | @yield('title')</title>
@@ -19,7 +19,7 @@
   </head>
   <body class="bg-light">
       @if(config('assets.common.appEnv') == 'production')
-        {{-- @yield('tag_manager_body') --}}
+        @yield('tag_manager_body')
       @endif
       @yield('bodyHeader')
       @if(config('assets.common.appEnv') == 'production')
--- resources/views/layouts/common/google.blade.php
+++ resources/views/layouts/common/google.blade.php
@@ -1,15 +1,15 @@
 @section('tag_manager_head')
-{{--    <!-- Google Tag Manager -->--}}
-{{--    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':--}}
-{{--                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],--}}
-{{--            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=--}}
-{{--            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);--}}
-{{--        })(window,document,'script','dataLayer','GTM-PMHCL93');</script>--}}
-{{--    <!-- End Google Tag Manager -->--}}
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-K9V6S2S3');</script>
+  <!-- End Google Tag Manager -->
 @endsection
 
 @section('analitics')
-    <!-- Global site tag (gtag.js) - Google Analytics -->
+    {{-- <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-XFDXK7PGHL"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
@@ -26,52 +26,16 @@
             });
         @endif
 
-    </script>
-
-
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    {{-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-108878672-4"></script>
-    <script>
-        //トラッキングコード
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'UA-108878672-4');
-
-        //user-idコード
-        @if(Auth::check())
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)
-                    [0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-            ga('create', 'UA-108878672-4', 'auto');
-            ga('set', '&uid', '{{Auth::id()}}'); // ログインしている user_id を使用してUser-ID を設定します。
-            ga('send', 'pageview');
-        @endif
-
     </script> --}}
-    
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-7BB8S0QLFQ"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'G-7BB8S0QLFQ');
-    </script>
-
+  
 @endsection
 
 
 @section('tag_manager_body')
-{{--    <!-- Google Tag Manager (noscript) -->--}}
-{{--    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PMHCL93"--}}
-{{--                      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>--}}
-{{--    <!-- End Google Tag Manager (noscript) -->--}}
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K9V6S2S3"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
 @endsection
 
 @section('adsense')
